9512b9325ff6665628e1e2d6c337a52d193bc0a5
aniegboka.chukwudi <aniegbokachukwudi@gmail.com>
2019-04-04 13:43:13 +0100

    fixed issues on

---
+++ src/Model/Entity/Fee.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+
+/**
+ * Fee Entity
+ *
+ * @property int $id
+ * @property string $name
+ * @property int $amount
+ * @property int $user_id
+ * @property int $status
+ *
+ * @property \App\Model\Entity\User $user
+ * @property \App\Model\Entity\Feeallocation[] $feeallocations
+ * @property \App\Model\Entity\Department[] $departments
+ */
+class Fee extends Entity
+{
+
+    /**
+     * Fields that can be mass assigned using newEntity() or patchEntity().
+     *
+     * Note that when '*' is set to true, this allows all unspecified fields to
+     * be mass assigned. For security purposes, it is advised to set '*' to false
+     * (or remove it), and explicitly make individual fields accessible as needed.
+     *
+     * @var array
+     */
+    protected $_accessible = [
+        'name' => true,
+        'amount' => true,
+        'user_id' => true,
+        'status' => true,
+        'user' => true,
+        'feeallocations' => true,
+        'departments' => true
+    ];
+}
