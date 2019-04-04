a490b2e5340c5c7bf8e756a11dfe23eb37c91a7b
aniegboka.chukwudi <aniegbokachukwudi@gmail.com>
2019-04-04 09:45:37 +0100

    fixed issues on

--- src/Controller/SubjectsController.php
+++ src/Controller/SubjectsController.php
@@ -1,149 +1,143 @@
 <?php
-namespace App\Controller;
 
-use App\Controller\AppController;
+  namespace App\Controller;
 
-/**
- * Subjects Controller
- *
- * @property \App\Model\Table\SubjectsTable $Subjects
- *
- * @method \App\Model\Entity\Subject[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
- */
-class SubjectsController extends AppController
-{
+  use App\Controller\AppController;
 
-    /**
-     * Index method
-     *
-     * @return \Cake\Http\Response|void
-     */
-    public function index()
-    {
-        $this->paginate = [
-            'contain' => ['Departments', 'Users']
-        ];
-        $subjects = $this->paginate($this->Subjects);
+  /**
+   * Subjects Controller
+   *
+   * @property \App\Model\Table\SubjectsTable $Subjects
+   *
+   * @method \App\Model\Entity\Subject[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+   */
+  class SubjectsController extends AppController {
 
-        $this->set(compact('subjects'));
-    }
+      /**
+       * Index method
+       *
+       * @return \Cake\Http\Response|void
+       */
+      public function index() {
+          $this->paginate = [
+              'contain' => ['Departments', 'Users']
+          ];
+          $subjects = $this->paginate($this->Subjects);
 
-    /**
-     * View method
-     *
-     * @param string|null $id Subject id.
-     * @return \Cake\Http\Response|void
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function view($id = null)
-    {
-        $subject = $this->Subjects->get($id, [
-            'contain' => ['Departments', 'Users']
-        ]);
+          $this->set(compact('subjects'));
+      }
 
-        $this->set('subject', $subject);
-        $this->viewBuilder()->setLayout('adminbackend');   
-    }
+      /**
+       * View method
+       *
+       * @param string|null $id Subject id.
+       * @return \Cake\Http\Response|void
+       * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+       */
+      public function view($id = null) {
+          $subject = $this->Subjects->get($id, [
+              'contain' => ['Departments', 'Teachers.Users']
+          ]);
+          
+          $this->set('subject', $subject);
+          $this->viewBuilder()->setLayout('adminbackend');
+      }
 
-    /**
-     * Add method
-     *
-     * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
-     */
-    public function add()
-    {
-        $subject = $this->Subjects->newEntity();
-        if ($this->request->is('post')) {
-            $subject = $this->Subjects->patchEntity($subject, $this->request->getData());
-            if ($this->Subjects->save($subject)) {
-                $this->Flash->success(__('The subject has been saved.'));
+      /**
+       * Add method
+       *
+       * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
+       */
+      public function add() {
+          $subject = $this->Subjects->newEntity();
+          if ($this->request->is('post')) {
+              $subject = $this->Subjects->patchEntity($subject, $this->request->getData());
+              if ($this->Subjects->save($subject)) {
+                  $this->Flash->success(__('The subject has been saved.'));
 
-                return $this->redirect(['action' => 'managesubjects']);
-            }
-            $this->Flash->error(__('The subject could not be saved. Please, try again.'));
-        }
-        $departments = $this->Subjects->Departments->find('list', ['limit' => 200]);
-        $users = $this->Subjects->Users->find('list', ['limit' => 200]);
-        $this->set(compact('subject', 'departments', 'users'));
-         $this->viewBuilder()->setLayout('adminbackend');
-    }
-        
-        
-    
-    
+                  return $this->redirect(['action' => 'managesubjects']);
+              }
+              $this->Flash->error(__('The subject could not be saved. Please, try again.'));
+          }
+          $departments = $this->Subjects->Departments->find('list', ['limit' => 200]);
+          $users = $this->Subjects->Users->find('list', ['limit' => 200]);
+          $this->set(compact('subject', 'departments', 'users'));
+          $this->viewBuilder()->setLayout('adminbackend');
+      }
 
-    /**
-     * Edit method
-     *
-     * @param string|null $id Subject id.
-     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Network\Exception\NotFoundException When record not found.
-     */
-    public function edit($id = null)
-    {
-        $subject = $this->Subjects->get($id, [
-            'contain' => []
-        ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $subject = $this->Subjects->patchEntity($subject, $this->request->getData());
-            if ($this->Subjects->save($subject)) {
-                $this->Flash->success(__('The subject has been saved.'));
+      /**
+       * Edit method
+       *
+       * @param string|null $id Subject id.
+       * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
+       * @throws \Cake\Network\Exception\NotFoundException When record not found.
+       */
+      public function edit($id = null) {
+          $subject = $this->Subjects->get($id, [
+              'contain' => []
+          ]);
+          if ($this->request->is(['patch', 'post', 'put'])) {
+              $subject = $this->Subjects->patchEntity($subject, $this->request->getData());
+              if ($this->Subjects->save($subject)) {
+                  $this->Flash->success(__('The subject has been saved.'));
 
-                return $this->redirect(['action' => 'managesubjects']);
-            }
-            $this->Flash->error(__('The subject could not be saved. Please, try again.'));
-        }
-        $departments = $this->Subjects->Departments->find('list', ['limit' => 200]);
-        $users = $this->Subjects->Users->find('list', ['limit' => 200]);
-        $this->set(compact('subject', 'departments', 'users'));
+                  return $this->redirect(['action' => 'managesubjects']);
+              }
+              $this->Flash->error(__('The subject could not be saved. Please, try again.'));
+          }
+          $departments = $this->Subjects->Departments->find('list', ['limit' => 200]);
+          $users = $this->Subjects->Users->find('list', ['limit' => 200]);
+          $this->set(compact('subject', 'departments', 'users'));
 
-        $this->viewBuilder()->setLayout('adminbackend');   
-    }
+          $this->viewBuilder()->setLayout('adminbackend');
+      }
 
-    /**
-     * Delete method
-     *
-     * @param string|null $id Subject id.
-     * @return \Cake\Http\Response|null Redirects to index.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function delete($id = null)
-    {
-        $this->request->allowMethod(['post', 'delete']);
-        $subject = $this->Subjects->get($id);
-        if ($this->Subjects->delete($subject)) {
-            $this->Flash->success(__('The subject has been deleted.'));
-        } else {
-            $this->Flash->error(__('The subject could not be deleted. Please, try again.'));
-        }
+      /**
+       * Delete method
+       *
+       * @param string|null $id Subject id.
+       * @return \Cake\Http\Response|null Redirects to index.
+       * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+       */
+      public function delete($id = null) {
+          $this->request->allowMethod(['post', 'delete']);
+          $subject = $this->Subjects->get($id);
+          if ($this->Subjects->delete($subject)) {
+              $this->Flash->success(__('The subject has been deleted.'));
+          } else {
+              $this->Flash->error(__('The subject could not be deleted. Please, try again.'));
+          }
 
-        return $this->redirect(['action' => 'index']);
-    }
-    
-    public function managesubjects()
-    {
-        $this->paginate = [
-            'contain' => ['Departments', 'Users']
-        ];
-        $subjects = $this->paginate($this->Subjects);
+          return $this->redirect(['action' => 'index']);
+      }
 
-        $this->set(compact('subjects'));
+      public function managesubjects() {
+          $this->paginate = [
+              'contain' => ['Departments', 'Users']
+          ];
+          $subjects = $this->paginate($this->Subjects);
 
-        $this->viewBuilder()->setLayout('adminbackend');    
-        
-    }
-    
-     public function changesubjectstatus($id, $status) {
-        $subjects = $this->Subjects->get($id);
-        $subjects->status = $status;
-        if ($status==1){$mystatus="Enabled";}
-        else if ($status==0){$mystatus="Disabled";}
-        
-        if ($this->Subjects->save($subjects)) {
-            $this->Flash->success(__('Subject status ' . $mystatus));
-        } else {
-            $this->Flash->error(__('Unable to change Subjects status. Please, try again.'));
-        }
-        return $this->redirect(['controller' => 'Subjects', 'action' => 'managesubjects']);
-    }
-}
+          $this->set(compact('subjects'));
+
+          $this->viewBuilder()->setLayout('adminbackend');
+      }
+
+      public function changesubjectstatus($id, $status) {
+          $subjects = $this->Subjects->get($id);
+          $subjects->status = $status;
+          if ($status == 1) {
+              $mystatus = "Enabled";
+          } else if ($status == 0) {
+              $mystatus = "Disabled";
+          }
+
+          if ($this->Subjects->save($subjects)) {
+              $this->Flash->success(__('Subject status ' . $mystatus));
+          } else {
+              $this->Flash->error(__('Unable to change Subjects status. Please, try again.'));
+          }
+          return $this->redirect(['controller' => 'Subjects', 'action' => 'managesubjects']);
+      }
+
+  }
+  
