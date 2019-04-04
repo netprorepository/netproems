545e94f30982f0862712ad01109252b4f555e517
aniegboka.chukwudi <aniegbokachukwudi@gmail.com>
2019-04-04 13:43:51 +0100

    fixed issues on

---
+++ src/Model/Table/FeesTable.php
@@ -0,0 +1,100 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Fees Model
+ *
+ * @property \App\Model\Table\UsersTable|\Cake\ORM\Association\BelongsTo $Users
+ * @property \App\Model\Table\FeeallocationsTable|\Cake\ORM\Association\HasMany $Feeallocations
+ * @property \App\Model\Table\DepartmentsTable|\Cake\ORM\Association\BelongsToMany $Departments
+ *
+ * @method \App\Model\Entity\Fee get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Fee newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\Fee[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Fee|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Fee|bool saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Fee patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Fee[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Fee findOrCreate($search, callable $callback = null, $options = [])
+ */
+class FeesTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('fees');
+        $this->setDisplayField('name');
+        $this->setPrimaryKey('id');
+
+        $this->belongsTo('Users', [
+            'foreignKey' => 'user_id',
+            'joinType' => 'INNER'
+        ]);
+        $this->hasMany('Feeallocations', [
+            'foreignKey' => 'fee_id'
+        ]);
+        $this->belongsToMany('Departments', [
+            'foreignKey' => 'fee_id',
+            'targetForeignKey' => 'department_id',
+            'joinTable' => 'departments_fees'
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmpty('id', 'create');
+
+        $validator
+            ->scalar('name')
+            ->maxLength('name', 98)
+            ->requirePresence('name', 'create')
+            ->notEmpty('name');
+
+        $validator
+            ->integer('amount')
+            ->requirePresence('amount', 'create')
+            ->notEmpty('amount');
+
+//        $validator
+//            ->integer('status')
+//            ->requirePresence('status', 'create')
+//            ->notEmpty('status');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->add($rules->existsIn(['user_id'], 'Users'));
+
+        return $rules;
+    }
+}
