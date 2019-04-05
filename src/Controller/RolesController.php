b86f3a9d22a04166a26172f649f33826a624d859
aniegboka.chukwudi <aniegbokachukwudi@gmail.com>
2019-04-05 15:16:07 +0100

    fixed issues on

--- src/Controller/RolesController.php
+++ src/Controller/RolesController.php
@@ -33,7 +33,7 @@ class RolesController extends AppController
      * @return \Cake\Http\Response|void
      * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
      */
-    public function view($id = null)
+    public function viewrole($id = null)
     {
         $role = $this->Roles->get($id, [
             'contain' => ['Users']
@@ -71,7 +71,7 @@ class RolesController extends AppController
      * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
      * @throws \Cake\Network\Exception\NotFoundException When record not found.
      */
-    public function edit($id = null)
+    public function editrole($id = null)
     {
         $role = $this->Roles->get($id, [
             'contain' => []
