7f466c1c4462d4dc95cc4fb175a42bf6945c1b94
aniegboka.chukwudi <aniegbokachukwudi@gmail.com>
2019-04-09 17:10:02 +0100

    added students module

---
+++ src/Model/Table/SessionsTable.php
@@ -0,0 +1,93 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Sessions Model
+ *
+ * @property \App\Model\Table\UsersTable|\Cake\ORM\Association\BelongsTo $Users
+ * @property \App\Model\Table\SettingsTable|\Cake\ORM\Association\HasMany $Settings
+ * @property \App\Model\Table\TransactionsTable|\Cake\ORM\Association\HasMany $Transactions
+ *
+ * @method \App\Model\Entity\Session get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Session newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\Session[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Session|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Session|bool saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Session patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Session[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Session findOrCreate($search, callable $callback = null, $options = [])
+ */
+class SessionsTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('sessions');
+        $this->setDisplayField('name');
+        $this->setPrimaryKey('id');
+
+        $this->belongsTo('Users', [
+            'foreignKey' => 'user_id',
+            'joinType' => 'INNER'
+        ]);
+        $this->hasMany('Settings', [
+            'foreignKey' => 'session_id'
+        ]);
+        $this->hasMany('Transactions', [
+            'foreignKey' => 'session_id'
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmpty('id', 'create');
+
+        $validator
+            ->scalar('name')
+            ->maxLength('name', 44)
+            ->requirePresence('name', 'create')
+            ->notEmpty('name');
+
+        $validator
+            ->dateTime('createdate')
+            ->requirePresence('createdate', 'create')
+            ->notEmpty('createdate');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->add($rules->existsIn(['user_id'], 'Users'));
+
+        return $rules;
+    }
+}
