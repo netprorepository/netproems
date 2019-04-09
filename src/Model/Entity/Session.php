558d702d90e1b5985a46104c708de59f974d8ac6
aniegboka.chukwudi <aniegbokachukwudi@gmail.com>
2019-04-09 17:48:15 +0100

    added students module

---
+++ src/Model/Entity/Session.php
@@ -0,0 +1,38 @@
+<?php
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+
+/**
+ * Session Entity
+ *
+ * @property int $id
+ * @property string $name
+ * @property int $user_id
+ * @property \Cake\I18n\FrozenTime $createdate
+ *
+ * @property \App\Model\Entity\User $user
+ * @property \App\Model\Entity\Setting[] $settings
+ * @property \App\Model\Entity\Transaction[] $transactions
+ */
+class Session extends Entity
+{
+
+    /**
+     * Fields that can be mass assigned using newEntity() or patchEntity().
+     *
+     * Note that when '*' is set to true, this allows all unspecified fields to
+     * be mass assigned. For security purposes, it is advised to set '*' to false
+     * (or remove it), and explicitly make individual fields accessible as needed.
+     *
+     * @var array
+     */
+    protected $_accessible = [
+        'name' => true,
+        'user_id' => true,
+        'createdate' => true,
+        'user' => true,
+        'settings' => true,
+        'transactions' => true
+    ];
+}
