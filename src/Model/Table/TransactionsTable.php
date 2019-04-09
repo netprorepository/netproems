0ad7b678c398343f9f444fc8ba449c69624e75be
aniegboka.chukwudi <aniegbokachukwudi@gmail.com>
2019-04-09 17:10:12 +0100

    added students module

---
+++ src/Model/Table/TransactionsTable.php
@@ -0,0 +1,109 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Transactions Model
+ *
+ * @property \App\Model\Table\StudentsTable|\Cake\ORM\Association\BelongsTo $Students
+ * @property |\Cake\ORM\Association\BelongsTo $Sessions
+ *
+ * @method \App\Model\Entity\Transaction get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Transaction newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\Transaction[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Transaction|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Transaction|bool saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Transaction patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Transaction[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Transaction findOrCreate($search, callable $callback = null, $options = [])
+ */
+class TransactionsTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('transactions');
+        $this->setDisplayField('id');
+        $this->setPrimaryKey('id');
+
+        $this->belongsTo('Students', [
+            'foreignKey' => 'student_id',
+            'joinType' => 'INNER'
+        ]);
+        $this->belongsTo('Sessions', [
+            'foreignKey' => 'session_id',
+            'joinType' => 'INNER'
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmpty('id', 'create');
+
+        $validator
+            ->dateTime('transdate')
+            ->requirePresence('transdate', 'create')
+            ->notEmpty('transdate');
+
+        $validator
+            ->scalar('amount')
+            ->maxLength('amount', 18)
+            ->requirePresence('amount', 'create')
+            ->notEmpty('amount');
+
+        $validator
+            ->scalar('paystatus')
+            ->maxLength('paystatus', 44)
+            ->requirePresence('paystatus', 'create')
+            ->notEmpty('paystatus');
+
+        $validator
+            ->scalar('payref')
+            ->maxLength('payref', 188)
+            ->requirePresence('payref', 'create')
+            ->notEmpty('payref');
+
+        $validator
+            ->scalar('gresponse')
+            ->maxLength('gresponse', 88)
+            ->requirePresence('gresponse', 'create')
+            ->notEmpty('gresponse');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->add($rules->existsIn(['student_id'], 'Students'));
+        $rules->add($rules->existsIn(['session_id'], 'Sessions'));
+
+        return $rules;
+    }
+}
