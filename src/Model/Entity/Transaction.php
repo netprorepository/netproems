8b341908d74ce476e7ee024b291cddadd95ae11f
aniegboka.chukwudi <aniegbokachukwudi@gmail.com>
2019-04-09 17:48:04 +0100

    added students module

---
+++ src/Model/Entity/Transaction.php
@@ -0,0 +1,46 @@
+<?php
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+
+/**
+ * Transaction Entity
+ *
+ * @property int $id
+ * @property int $student_id
+ * @property \Cake\I18n\FrozenTime $transdate
+ * @property string $amount
+ * @property string $paystatus
+ * @property string $payref
+ * @property string $gresponse
+ * @property int $session_id
+ * @property int $fee_id
+ *
+ * @property \App\Model\Entity\Student $student
+ * @property \App\Model\Entity\Session $session
+ */
+class Transaction extends Entity
+{
+
+    /**
+     * Fields that can be mass assigned using newEntity() or patchEntity().
+     *
+     * Note that when '*' is set to true, this allows all unspecified fields to
+     * be mass assigned. For security purposes, it is advised to set '*' to false
+     * (or remove it), and explicitly make individual fields accessible as needed.
+     *
+     * @var array
+     */
+    protected $_accessible = [
+        'student_id' => true,
+        'transdate' => true,
+        'amount' => true,
+        'paystatus' => true,
+        'payref' => true,
+        'gresponse' => true,
+        'session_id' => true,
+        'fee_id' => true,
+        'student' => true,
+        'session' => true
+    ];
+}
