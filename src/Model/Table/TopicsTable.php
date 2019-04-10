aae1d8e5f36a9492ea78f5eb2da268815d793aca
aniegboka.chukwudi <aniegbokachukwudi@gmail.com>
2019-04-10 15:47:17 +0100

    added students module

---
+++ src/Model/Table/TopicsTable.php
@@ -0,0 +1,102 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Topics Model
+ *
+ * @property \App\Model\Table\SubjectsTable|\Cake\ORM\Association\BelongsTo $Subjects
+ * @property \App\Model\Table\UsersTable|\Cake\ORM\Association\BelongsTo $Users
+ *
+ * @method \App\Model\Entity\Topic get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Topic newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\Topic[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Topic|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Topic|bool saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Topic patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Topic[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Topic findOrCreate($search, callable $callback = null, $options = [])
+ */
+class TopicsTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('topics');
+        $this->setDisplayField('title');
+        $this->setPrimaryKey('id');
+
+        $this->belongsTo('Subjects', [
+            'foreignKey' => 'subject_id',
+            'joinType' => 'INNER'
+        ]);
+        $this->belongsTo('Users', [
+            'foreignKey' => 'user_id',
+            'joinType' => 'INNER'
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmpty('id', 'create');
+
+        $validator
+            ->scalar('contents')
+            ->maxLength('contents', 4294967295)
+            ->requirePresence('contents', 'create')
+            ->notEmpty('contents');
+
+//        $validator
+//            ->dateTime('uploaddate')
+//            ->requirePresence('uploaddate', 'create')
+//            ->notEmpty('uploaddate');
+
+        $validator
+            ->scalar('updatedon')
+            ->maxLength('updatedon', 44)
+            ->allowEmpty('updatedon');
+
+        $validator
+            ->scalar('title')
+            ->maxLength('title', 160)
+            ->requirePresence('title', 'create')
+            ->notEmpty('title');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->add($rules->existsIn(['subject_id'], 'Subjects'));
+        $rules->add($rules->existsIn(['user_id'], 'Users'));
+
+        return $rules;
+    }
+}
