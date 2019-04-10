9273a3af8e7f7bc6a8361045c159ccc91b99a714
aniegboka.chukwudi <aniegbokachukwudi@gmail.com>
2019-04-10 16:07:32 +0100

    added students module

---
+++ webroot/js/summernote.init.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function(){
+
+    $('.summernote').summernote({
+        height: 244,                 // set editor height
+        minHeight: null,             // set minimum height of editor
+        maxHeight: null,             // set maximum height of editor
+        focus: false                 // set focus to editable area after initializing summernote
+    });
+});
