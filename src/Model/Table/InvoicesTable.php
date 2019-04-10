cbc2dae413ca5522ab7634eaa6108ac109d783ba
aniegboka.chukwudi <aniegbokachukwudi@gmail.com>
2019-04-10 12:42:36 +0100

    added students module

---
+++ src/Model/Table/InvoicesTable.php
@@ -0,0 +1,101 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Invoices Model
+ *
+ * @property \App\Model\Table\FeesTable|\Cake\ORM\Association\BelongsTo $Fees
+ * @property \App\Model\Table\StudentsTable|\Cake\ORM\Association\BelongsTo $Students
+ *
+ * @method \App\Model\Entity\Invoice get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Invoice newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\Invoice[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Invoice|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Invoice|bool saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Invoice patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Invoice[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Invoice findOrCreate($search, callable $callback = null, $options = [])
+ */
+class InvoicesTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('invoices');
+        $this->setDisplayField('id');
+        $this->setPrimaryKey('id');
+
+        $this->belongsTo('Fees', [
+            'foreignKey' => 'fee_id',
+            'joinType' => 'INNER'
+        ]);
+        $this->belongsTo('Students', [
+            'foreignKey' => 'student_id',
+            'joinType' => 'INNER'
+        ]);
+         $this->belongsTo('Sessions', [
+            'foreignKey' => 'session_id',
+            'joinType' => 'INNER'
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmpty('id', 'create');
+
+//        $validator
+//            ->dateTime('createdate')
+//            ->requirePresence('createdate', 'create')
+//            ->notEmpty('createdate');
+
+        $validator
+            ->scalar('amount')
+            ->maxLength('amount', 44)
+            ->requirePresence('amount', 'create')
+            ->notEmpty('amount');
+
+        $validator
+            ->scalar('paystatus')
+            ->maxLength('paystatus', 44)
+            ->requirePresence('paystatus', 'create')
+            ->notEmpty('paystatus');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->add($rules->existsIn(['fee_id'], 'Fees'));
+        $rules->add($rules->existsIn(['student_id'], 'Students'));
+
+        return $rules;
+    }
+}
