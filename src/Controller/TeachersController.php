248596ca0c0e8d2753c385593e12ebcf6aafc63b
aniegboka.chukwudi <aniegbokachukwudi@gmail.com>
2019-05-01 14:42:02 +0100

    worked on student module

--- src/Controller/TeachersController.php
+++ src/Controller/TeachersController.php
@@ -1,7 +1,11 @@
 <?php
 
   namespace App\Controller;
-
+ use Cake\Routing\Router;
+  use PhpOffice\PhpSpreadsheet\Spreadsheet;
+  use PhpOffice\PhpSpreadsheet\IOFactory;
+  use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+  use PhpOffice\PhpSpreadsheet\Helper;
   use Cake\Mailer\Email;
   use Cake\ORM\TableRegistry;
   use App\Controller\AppController;
@@ -78,6 +82,20 @@
           readfile("cvs/" . $teacher->cv);
           return;
       }
+
+      
+      
+    
+
+      
+
+
+
+
+
+
+
+
 
       /**
        * Add method
@@ -161,15 +179,19 @@
       
       //the teachers dashboard
       public function dashboard() {
-          $teacher = $this->Teachers->find()->where(['user_id' => $this->Auth->user('id')])->first();
+          $teacher = $this->Teachers->find()
+                  ->contain(['Subjects','Departments','Departments.Students','Departments.Subjects'])
+                  ->where(['user_id' => $this->Auth->user('id')])->first();
           if (empty($teacher)) {
               $this->Flash->error(__('Sorry, you need to set up your profile first.'));
 
               return $this->redirect(['action' => 'newprofile']);
           } else {
               $this->Flash->success(__('Welcome!.'));
+               $student_Table = TableRegistry::get('Students');
+               $students = $student_Table->find()->where(['status'=>'Admitted'])->count();
           }
-          $this->set(compact('teacher'));
+          $this->set(compact('teacher','students'));
           $this->viewBuilder()->setLayout('adminbackend');
       }
 
@@ -610,5 +632,156 @@
           $this->set(compact('states'));
           //debug(json_encode($states , JSON_PRETTY_PRINT)); exit;
       }
+      
+      
+      //teachers method for uploading their results
+      public function uploadresults(){
+          $teacher = $this->Teachers->find()->contain(['Subjects'])->where(['user_id'=>$this->Auth->user('id')])->first();
+         if(!$teacher){
+              $this->Flash->error(__('Wrong access type'));
+             return $this->redirect(['controller'=>'Students','action' => 'index']);
+         }
+          $resultsTable = TableRegistry::get('Results');
+           if ($this->request->is(['patch', 'post', 'put'])) {
+              $faculty_id = $this->request->data('faculty_id');
+              $department_id = $this->request->data('department_id');
+              // $program_id = $this->request->data('program_id');
+              $session_id = $this->request->data('session_id');
+              $semester_id = $this->request->data('semester_id');
+              $course_id = $this->request->data('subject_id');
+
+
+              $filename = $this->request->data['result']['name'];
+              $ext = pathinfo($filename, PATHINFO_EXTENSION);
+              // echo $ext; exit;
+              $allowedext = ['csv', 'xlsx'];
+              if ($this->request->data['result']['error']) {
+                  $this->Flash->error(__('Sorry, there is a problem with the file,only csv or xlsx files can be uploaded. Please check and try again'));
+
+                  return $this->redirect(['action' => 'uploadresults']);
+              }
+              if (!in_array($ext, $allowedext)) {
+                  $this->Flash->error(__('Sorry, only csv or xlsx files can be uploaded.'));
+
+                  return $this->redirect(['action' => 'uploadresults']);
+              } else {
+                  $helper = new Helper\Sample();
+                  debug($helper);
+                  $inputFileName = $this->request->data['result']['tmp_name'];
+                  $spreadsheet = IOFactory::load($inputFileName);
+                  $sheetData = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
+
+                  $count = 0;
+                  $inserted = 0;
+                  $duplicate_results = 0;
+                  $unknown_students = 0;
+                  foreach ($sheetData as $data) {
+
+                      $count++;
+
+                      if ($count > 1) {
+                          // debug(json_encode( $data, JSON_PRETTY_PRINT)); exit;
+                          //  $programe = $programes_table->get($program_id);
+                          $department =  $resultsTable->Departments->get($department_id);
+                          $semester =  $resultsTable->Semesters->get($semester_id);
+                          $course =  $resultsTable->Subjects->get($course_id);
+                          $session =  $resultsTable->Sessions->get($session_id);
+
+//               echo strtolower(trim($semester->name)) .' '. strtolower(trim($data['H'])).'<br />';
+//              echo strtolower(trim($department->name)) .' '. strtolower(trim($data['G'])).'<br />';
+//             echo  strtolower(trim($programe->programname)) .' '. strtolower(trim($data['F'])).'<br />';
+//             echo  strtolower(trim($course->name)) .' '. strtolower(trim($data['F'])).'<br />';
+//            echo   strtolower(trim($session->name)) .' '. strtolower(trim($data['I'])).'<br />';
+//            exit;
+                          if (strtolower(trim($department->name)) === strtolower(trim($data['G'])) &&
+                                  strtolower(trim($course->name)) === strtolower(trim($data['F'])) &&
+                                  strtolower(trim($semester->name)) === strtolower(trim($data['H'])) &&
+                                  strtolower(trim($session->name)) === strtolower(trim($data['I']))) {
+
+                              //get the student and ensure no double result
+                              //  debug(json_encode( $data, JSON_PRETTY_PRINT)); exit;
+                              $student =  $resultsTable->Students->find()->where(['regno' => $data['A']])->first();
+                              //ensure no result for this course already
+
+                              if ($student) {
+
+                                  $has_result =  $resultsTable->find()->where(['regno' => $data['A'],
+                                      'department_id' => $department_id, 'subject_id' => $course_id, 'semester_id' => $semester_id, 'session_id' => $session_id]);
+                                  if (empty($has_result->toArray())) {
+                                      // debug(json_encode( $data, JSON_PRETTY_PRINT)); exit;
+                                      //create a new result for this student
+                                      $result =  $resultsTable->newEntity();
+                                      $result->regno = $data['A'];
+                                      // $result->surname = $data['B'];
+                                      $result->student_id = $student->id;
+                                      $result->faculty_id = $faculty_id;
+                                      // $result->firstname = $data['C'];
+                                      // $result->middlename = $data['D'];
+                                      $result->department_id = $department_id;
+                                      $result->remark = $data['E'];
+                                      $result->semester_id = $semester_id;
+                                      $result->subject_id = $course_id;
+                                      $result->session_id = $session_id;
+                                      $result->creditload = $data['D'];
+                                      $result->score = $data['B'];
+                                      $result->grade = $data['C'];
+                                      $result->user_id = $this->Auth->user('id');
+                                      // debug(json_encode($result, JSON_PRETTY_PRINT)); exit;
+                                       $resultsTable->save($result);
+                                      $inserted ++;
+                                  } else {
+                                      $duplicate_results++;
+                                  }
+                              } else {
+                                  $unknown_students++;
+                              }
+                          } else {
+                              $this->Flash->error('Total results uploaded : ' . $inserted . ' Some results failed to upload because selected data didnt match provided data. Please ensure the right department,'
+                                      . 'course, session and faculty was selected. Duplicate results found : ' . $duplicate_results
+                                      . ' Unknown students : ' . $unknown_students);
+                              return $this->redirect(['action' => 'uploadresults']);
+                          }
+
+                          // debug(json_encode($data['F'], JSON_PRETTY_PRINT)); exit;
+                      }
+                  }
+                  //log activity
+                  $usercontroller = new UsersController();
+
+                  $title = "Result Bulk Upload ";
+                  $user_id = $this->Auth->user('id');
+                  $description = "Uploaded " . $inserted . ' results';
+                  $ip = $this->request->clientIp();
+                  $type = "Add";
+                  $usercontroller->makeLog($title, $user_id, $description, $ip, $type);
+                  $this->Flash->success(__($inserted . ' Result(s) have been uploaded successfully. Duplicates found : ' . $duplicate_results . ' Unknown students : ' . $unknown_students));
+
+                  return $this->redirect(['action' => 'uploadresults']);
+              }
+          }
+          
+          //get teacher subjects
+          $teacher_subjects = [];
+          foreach ($teacher->subjects as $subject){
+              array_push($teacher_subjects, $subject->id);
+          }
+          $faculties =  $resultsTable->Faculties->find('list', ['limit' => 200])
+                  ->order(['name' => 'ASC']);
+          $departments =  $resultsTable->Departments->find('list', ['limit' => 200])
+                  ->order(['name' => 'ASC']);
+          $subjects =  $resultsTable->Subjects->find('list', ['limit' => 200])
+                  ->where(['id IN '=>$teacher_subjects])
+                  ->order(['name' => 'ASC']);
+          $semesters =  $resultsTable->Semesters->find('list', ['limit' => 200]);
+          $sessions =  $resultsTable->Sessions->find('list', ['limit' => 200]);
+          $this->viewBuilder()->setLayout('adminbackend');
+          $this->set(compact('result', 'students', 'faculties', 'departments', 'subjects', 'semesters', 'sessions', 'users'));
+      }
+  
+      
+      
+      
+      
+      
   }
   
